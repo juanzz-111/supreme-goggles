b9b03e86f4a29e5e95110a9695809354b87deb71
juanzz-111 <juanzitoplays15@gmail.com>
2026-09-10 22:43:33 +0000

    remove arquivos desnecessários e adiciona funcionalidade de calculadora ao aplicativo Flask

--- meu-app/app.py
+++ meu-app/app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def inicio():
@@ -9,6 +10,34 @@ def inicio():
 @app.route("/sobre")
 def sobre():
     return render_template("sobre.html")
+
+@app.route("/calculadora")
+def calculadora():
+    return render_template("calculadora.html")
+
+@app.route("/resultado", methods=["POST"])
+def resultado():
+
+    numero1 = float(request.form["numero1"])
+    numero2 = float(request.form["numero2"])
+    operacao = request.form["operacao"]
+
+    if operacao == "+":
+        resultado = numero1 + numero2
+
+    elif operacao == "-":
+        resultado = numero1 - numero2
+
+    elif operacao == "*":
+        resultado = numero1 * numero2
+
+    elif operacao == "/":
+        if numero2 != 0:
+            resultado = numero1 / numero2
+        else:
+            resultado = "Aqui tem como não rapaz, IMPOSSÍVEL divisível por zero"
+
+    return render_template("resultado.html", resultado=resultado)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001, debug=True)
